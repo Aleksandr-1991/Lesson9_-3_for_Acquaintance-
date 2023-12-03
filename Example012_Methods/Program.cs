dbf84b99c643fd95993d106741754ed4d7a59c71
Aleks37 <lao35180972643335589007>
2023-12-03 15:59:59 +0400

    Add changes in Example012_Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,119 +1,107 @@
-﻿using System;
-// 1 вид методов:
-// void Method1 ()
-// {
-//     Console.WriteLine("Автор ...");
-// }
+﻿// То что закомиченно двойным слешем // Не раскомичивать Никогда.
+// То что закомиченно слешем со /* и */  раскомичивать, удаляя только первый /* либо удаляя Таб перед ним:
+// 1 Вид методов: 
+/*
+void Method1 ()
+{
+    Console.WriteLine("Автор ...");
+}
 
-// Method1 ();
+Method1 ();
+// */
 
-// 2 вид методов:
-// void Method2 (string msg)
-// {
-//     Console.WriteLine(msg);
-// }
-// Method2 ("Тест сообщения.");
+//  2 Вид методов: 
+/*
+void Method2 (string msg, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(msg);
+    }
+  
+}
+Method2 (msg: "Тест.", count: 4);
+Method2 (count: 5, msg: "Тест новый.");   // Это "именованые аргументы" - их не обязательно писать по порядку.
+// */
 
-// void Method21 (string msg, int count)
-// {
-//     int i=0;
-//     while (i<count)
-//     {
-//         Console.WriteLine(msg);
-//         i++;
-//     }
-// }
-// Method21 (msg: "Тест.", count: 5);
-// Method21 (count: 5, msg: "Тест новый.");
+//  3 Вид методов: 
+/*
+int Method3 ()
+{
+    return DateTime.Now.Year;
+}
+Console.Write (Method3());
+// */
 
-// 3 вид методов:
-// int Method3 ()
-// {
-//     return DateTime.Now.Year;
-// }
-// int year = Method3();
-// Console.WriteLine(year);
+//  4 Вид методов: 
+/*
+string Method4 (int count, string text)
+{
+    string result = String.Empty;  // string result = "";
+    for (int i=0; i<count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4 (7, "Некий текст. ");
+Console.Write(res);
+// */
 
-// // 4 Вид методов:
-// string Method4 (int count, string text)
-// {   int i=0;
-//     string result = String.Empty;
-//     while (i<count)
-//     {
-//         result = result + text;
-//         i++;
-//     }
-//     return result;
-// }
-// string res = Method4 (10, "Некий текст. ");
-// Console.WriteLine(res);
+//  Цикл в цикле! 
+/*
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+// */
 
-// 4 Вид методов ч/з метод  for:
-// string Method4 (int count, string text)
-// {
-//     string result = String.Empty;
-//     for (int i=0; i<count; i++)
-//     {
-//         result = result + text;
-//     }
-//     return result;
-// }
-// string res = Method4 (10, "Некий текст. ");
-// Console.WriteLine(res);
-// // Цикл в цикле:
-// for (int i = 2; i <= 10; i++)
-// {
-//     for (int j = 2; j <= 10; j++)
-//     {
-//         Console.WriteLine($"{i} x {j} = {i * j}");
-//     }
-//     Console.WriteLine();
-// }
+//  Работа с текстом!
+//  Теория:
+//  string s = "qwerty" - если дан такой текст, то
+//              012345  - это индексы символов в нём,
+//  s[3] это "r"        - ес написать s[3], то символ в переменной "s" c инедксом "3" имеет значение "r"
 
-// Работа с текстом.   Дан текст. Внём нужно заменить все пробелы чёрточками,
-// маленькие буквы "к" заменить большими "К",
-// а большие буквы "С" заменить маленькими "с".
-// string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
-//             + "ежели бы вас послали вместо нашего милого Винценгероде,"
-//             + "вы бы взяли приступом согласие русского короля. "
-//             + "Вы так красноречивы. Вы дадите мне чаю?";
-// // string s = "qwerty"
-// //             012345
-// // s[3] это "r"
+//  Задача.  Дан текст. Заменить все пробелы чёрточками, маленькие буквы "к" заменить большими "К", 
+//  а большие буквы "С" заменить маленькими "с".
+//  (удалить табуляцию здесь-ниже, чтобы раскомитить всю задачу): 
+/*
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие русского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace (text, ' ', '|'); 
+// объявляется предварительная переменная. Она нужна чтобы полученный текст не только был напечатан,
+// но и сохранить его в переменную, для дальней работы с ним, но уже с внесёнными изменениями.
+Console.WriteLine(newText);
+Console.WriteLine();  // пустая строка для красоты.
+Console.WriteLine(Replace(newText, 'к', 'К'));
+Console.WriteLine();  // пустая строка для красоты.
+Console.WriteLine(Replace(newText, 'о', 'А'));           // */
 
-// string Replace(string text, char oldValue, char newValue)
-// {
-//     string result = String.Empty;
-//     int length = text.Length;
-//     for (int i = 0; i < length; i++)
-//     {
-//         if (text[i] == oldValue) result = result + $"{newValue}";
-//         else result = result + $"{text[i]}";
-//     }
-
-//     return result;
-// }
-// // string newText = Replace (text, ' ', '|');
-// string newText = Replace(text, ' ', '|');
-// Console.WriteLine(newText);
-// Console.WriteLine();
-// newText = Replace(newText, 'к', 'К');
-// Console.WriteLine(newText);
-// Console.WriteLine();
-// newText = Replace(newText, 'о', 'А');
-// Console.WriteLine(newText);
-
-// ЗАДАЧА ПО СОРТИРОВКЕ МАССИВА!!!
+//  ЗАДАЧА ПО СОРТИРОВКЕ МАССИВА!!!  
+/*  
 int[] arr = {7, 4, 3, 8, 2, 6, 8, 5, 7, 9, 1, 3, 5, 4};
-
 
 void PrintArray (int[] array)
 {
     int count = array.Length;
-    for (int i = 0; i < count; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
+    for (int i = 0; i < count; i++)  Console.Write($"{array[i]} ");
     Console.WriteLine();
 }
 
@@ -122,7 +110,7 @@ void SelectionSort(int[] array)
     for (int i = 0; i < array.Length - 1; i++)
     {
         int minPosition = i;
-        for (int j = i+1; j < array.Length; j++)
+        for (int j = i + 1; j < array.Length; j++)
         {
             if (array[j] < array[minPosition]) minPosition = j;
         }
@@ -134,3 +122,4 @@ void SelectionSort(int[] array)
 PrintArray(arr);
 SelectionSort(arr);
 PrintArray(arr);
+*/
